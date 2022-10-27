ee5cf2a5115c7bbf9be2aca479bebcaf2b8dbadc
vladliaskivskyi <38475511+vlyaskovskij@users.noreply.github.com>
2022-10-27 10:12:55 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
-"use string";
-// localStorage.clear();
-// Переменная сколько привычки рендерить
 let howmany = localStorage.getItem("howm");
 
-// Рендерим привычки
 for (let i = 0; i < howmany; i++) {
   renderStart();
   let inputHabit = document.querySelectorAll(".habit__input");
@@ -23,7 +19,6 @@ const btnHabit = document.querySelectorAll(".habit-item__btn");
 const inputTask = document.querySelectorAll(".tasks__input");
 const addHabit = document.querySelector(".addhabit");
 
-// Функция переключает на готовую привычку
 document.body.addEventListener("click", function (event) {
   const elem = event.target.closest(".habit-item__btn");
   if (elem) {
@@ -38,12 +33,11 @@ document.body.addEventListener("click", function (event) {
   }
 });
 
-// Функция вписывает данные
 function getinput() {
   for (i = 0; i < inputTask.length; i++) {
     inputTask[i].value = localStorage.getItem(`task${i}`);
   }
-  // Переключаем галочку при входе
+
   for (i = 0; i < btnTask.length; i++) {
     if (localStorage.getItem(`btnt${i}`) == "done") {
       btnTask[i].classList.add("task-done");
@@ -61,11 +55,10 @@ function getinput() {
   }
 }
 
-// Получаем дату прошлого входа
 let dateCheck = localStorage.getItem("datecheck");
-// Получаем дату которая сейчас
+
 let dateNow = new Date().toLocaleDateString();
-// Проверяем совпадают ли даты
+
 if (dateNow !== dateCheck) {
   for (i = 0; i < inputTask.length; i++) {
     inputTask[i].value = "";
@@ -86,14 +79,12 @@ if (dateNow !== dateCheck) {
   localStorage.setItem("datecheck", dateCheck);
 }
 
-// Функция сохраняет данные при вводе
 function saveTask() {
   for (i = 0; i < inputTask.length; i++) {
     localStorage.setItem(`task${i}`, inputTask[i].value);
   }
 }
 
-// Функция переключает на готовую задачу
 for (let i = 0; i < btnTask.length; i++) {
   btnTask[i].addEventListener("click", function () {
     btnTask[i].classList.toggle("task-done");
@@ -106,10 +97,8 @@ for (let i = 0; i < btnTask.length; i++) {
   });
 }
 
-// Функция рендера предыдущих привычек
 function renderStart() {
   const element = document.createElement("div");
-
   element.innerHTML = `
             <div class="habit-item-wrapper">
               <buttton class="habit-item__btn"></buttton>
@@ -125,10 +114,8 @@ function renderStart() {
   document.querySelector(".habits-items").prepend(element);
 }
 
-// Отслеживаем нажатие на добавление привычки
 addHabit.addEventListener("click", render);
 
-// Функция добавления привычки
 function render() {
   const element = document.createElement("div");
 
@@ -150,7 +137,6 @@ function render() {
   return howmany;
 }
 
-// Функция сохраняет данные при вводе
 function saveHabit() {
   const inputHabit = document.querySelectorAll(".habit__input");
   for (i = 0; i < inputHabit.length; i++) {
